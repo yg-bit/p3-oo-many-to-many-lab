37ef4d41710c14e57f8bcd41f2c6453a6d7f45a9
yg-bit <jamesisaiah164@gmail.com>
2025-11-29 18:59:55 +0300

    Implement Author, Book, and Contract classes for many-to-many lab

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,61 @@
 class Author:
-    pass
+    all = []
+
+    def __init__(self, name):
+        if not isinstance(name, str):
+            raise Exception("name must be a string")
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        return [c for c in Contract.all if c.author is self]
+
+    def books(self):
+        return [c.book for c in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+
+    def total_royalties(self):
+        return sum(c.royalties for c in self.contracts())
 
 
 class Book:
-    pass
+    all = []
+
+    def __init__(self, title):
+        if not isinstance(title, str):
+            raise Exception("title must be a string")
+        self.title = title
+        Book.all.append(self)
+
+    def contracts(self):
+        return [c for c in Contract.all if c.book is self]
+
+    def authors(self):
+        return [c.author for c in self.contracts()]
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception("author must be an Author instance")
+        if not isinstance(book, Book):
+            raise Exception("book must be a Book instance")
+        if not isinstance(date, str):
+            raise Exception("date must be a string")
+        if not isinstance(royalties, int):
+            raise Exception("royalties must be an int")
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+
+        Contract.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [c for c in cls.all if c.date == date]
