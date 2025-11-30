0f9b997208ff5608ff10e4c3359b6a7bde4cbe61
yg-bit <jamesisaiah164@gmail.com>
2025-11-30 22:41:53 +0300

    Add property setters with validation to Contract class

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -49,12 +49,52 @@ class Contract:
         if not isinstance(royalties, int):
             raise Exception("royalties must be an int")
 
-        self.author = author
-        self.book = book
-        self.date = date
-        self.royalties = royalties
+        self._author = author
+        self._book = book
+        self._date = date
+        self._royalties = royalties
 
         Contract.all.append(self)
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise Exception("author must be an Author instance")
+        self._author = value
+
+    @property
+    def book(self):
+        return self._book
+
+    @book.setter
+    def book(self, value):
+        if not isinstance(value, Book):
+            raise Exception("book must be a Book instance")
+        self._book = value
+
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, value):
+        if not isinstance(value, str):
+            raise Exception("date must be a string")
+        self._date = value
+
+    @property
+    def royalties(self):
+        return self._royalties
+
+    @royalties.setter
+    def royalties(self, value):
+        if not isinstance(value, int):
+            raise Exception("royalties must be an int")
+        self._royalties = value
 
     @classmethod
     def contracts_by_date(cls, date):
